47ea37fce55aea7ab7f9318db99c3f283605d4cc
ivanburenkov <85966174+ivanburenkov@users.noreply.github.com>
2023-02-01 14:51:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -286,14 +286,14 @@ function runCcode() {
       domain: [0, 0.84],
       showgrid: false,
       showline: true,
-      title: "Signal number of photons",
+      title: "Idler number of photons",
       zeroline: false
     },
     yaxis: {
       domain: [0, 0.84],
       showgrid: false,
       showline: true,
-      title: "Idler number of photons",
+      title: "Signal number of photons",
       zeroline: false
     },
     xaxis2: {
